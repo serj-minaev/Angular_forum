b79e4f4effa1315707c3369a80831464b1b8880b
miner <miner_x@bk.ru>
2020-03-03 20:40:21 +0300

    first commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatMenuModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatGridListModule,
+  MatTabsModule,
+  MatAutocompleteModule,
+  MatInputModule
+]
+
+@NgModule({
+  declarations: [ ],
+  imports: [
+    CommonModule,
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
